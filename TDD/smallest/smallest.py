f5a147580df85cfdb30c217c1bad816c0c507655
wtc <wtc@pop-os.localdomain>
2024-09-30 13:22:47 +0200

    folders added

--- TDD/smallest/smallest.py
+++ TDD/smallest/smallest.py
@@ -1,4 +1,4 @@
-numbers =[20029,6267,7269,65,6363,839,9272]
+# numbers =[20029,6267,7269,65,6363,839,9272]
 
 def smallest(numbers):
     small=numbers[0]
@@ -7,4 +7,4 @@ def smallest(numbers):
             small=number
     return small
 
-smallest(numbers)
+# smallest(numbers)
