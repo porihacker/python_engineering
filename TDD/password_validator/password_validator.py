c85a4aefe0cce1e617063681a943d9b4088d0975
aloysathekge <sathekgealoy@gmail.com>
2024-10-01 14:34:39 +0200

    seeing the light

--- TDD/password_validator/password_validator.py
+++ TDD/password_validator/password_validator.py
@@ -9,13 +9,18 @@ special_chars = [
 
 def is_password_secure(password):
     # Initialize counter for special characters
-    
     special_char_count = 0
     lower_count=0
     upper_count=0
     digit_count=0
+    password_is_greater=True
     
     # Check each character in the password
+    if len(password)>=8:
+        password_is_greater=True
+    else:
+        password_is_greater=False
+        
     for char in password:
         if char in special_chars:
             special_char_count += 1
@@ -31,13 +36,13 @@ def is_password_secure(password):
     has_lower=lower_count>=1
     has_upper=upper_count>=1
     
-    is_secure=(has_special and has_digit and has_lower and has_upper)
+    is_secure=(password_is_greater and has_special and has_digit and has_lower and has_upper)
     return is_secure
 
    
        
 
 # Test the function
-password = "fghjddjjdj2dQ?"
+password = "fgT!3dd1"
 count = is_password_secure(password)
 print(f"The password is secure? {count}" )
