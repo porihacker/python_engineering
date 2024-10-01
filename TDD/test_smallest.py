2f0ef7df9f5cf78e739ab7f380c6bba7c769a8c7
aloysathekge <sathekgealoy@gmail.com>
2024-10-01 13:25:16 +0200

    new tdd

--- TDD/test_smallest.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from smallest import smallest
-
-class TestSmallest(unittest.TestCase):
-    def test_smallest(self):
-        self.assertEqual(smallest([1, 2, 3]), 1)
-
-    def test_smallest_negative(self):
-        self.assertEqual(smallest([-1, -2, -3]), -3)
